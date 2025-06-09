74fdcfefa059156f3a99fec43b85d2052029530e
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-09 20:16:04 +0100

    Get App Data

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ const controller = require("../controllers");
 // auth
 router.post("/register-vendor", controller.registerVendor);
 
+// krane
+
+router.get("/get-app-data", (req, res) => {
+	res.json(JSON.parse(process.env.DATA)[1]);
+});
 
 module.exports = router;
