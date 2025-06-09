981f3493eeb043834ddedbf0c80d770419351abe
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-09 17:38:47 +0100

    Data Response

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
+
 app.get("/data", (req, res) => {
-	console.log(process.env.DATA);
 	res.json(JSON.parse(process.env.DATA));
 });
 
