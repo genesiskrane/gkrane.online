f46f1697c74eb58a75d6ddf9630671fff6793809
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-28 06:21:25 +0100

    Data Added

--- routes/index.js
+++ routes/index.js
@@ -2,81 +2,22 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers");
 
-let data;
-const apps = [
-	{
-		name: "Kreative",
-		client: [{ name: "pro" }],
-	},
-	{
-		name: "Mart",
-		client: [
-			{
-				name: "store",
-			},
-			{ name: "shop" },
-		],
-	},
-];
+let apps;
 
 // auth
 router.post("/register-vendor", controller.registerVendor);
 
-// krane
 
-router.get("/krane/get-app-data", (req, res) => {
+router.get("/get-client-data", (req, res) => {
 	const name = req.query.name;
 
-	apps.forEach((app) => {
-		app.apiURL = process.env.CORE;
-	});
-
-	const app = apps.find((app) => app.name == name);
-	res.json(app);
-});
-
-router.get("/krane/get-client-data", (req, res) => {
-	const name = req.query.name;
-
-	const apps = [
-		{
-			name: "Kreative",
-			config: {
-				name: "Kreative",
-			},
-		},
-		{
-			name: "Mart",
-			config: {
-				name: "Mart",
-			},
-		},
-		{
-			name: "Store",
-			config: {
-				name: "Store",
-			},
-		},
-		{
-			name: "Shop",
-			config: {
-				name: "Shop",
-			},
-		},
-		{
-			name: "Pro",
-			config: {
-				name: "Pro",
-			},
-		},
-	];
 
 	const app = apps.find((app) => app.name == name);
 	res.json(app);
 });
 
 router.post("/data", (req, res) => {
-	data = req.body;
+	apps = req.body;
 	res.send("Saved");
 });
 
