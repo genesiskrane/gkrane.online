ede1fc6ffa7f5f53c66deb69e5385efe80e7e7cb
Genesis Krane <genesiskrane@gmail.com>
2025-07-30 16:47:28 +0100

    Server Modified

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,19 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers");
 
-let apps = [
-	{
-		name: "GKrane",
-		description: "Genesis Krane's personal website",
-		url: "https://gkrane.online",
-	},
-	{
-		name: "GKrane API Server",
-		description: "API server for GKrane",
-		url: "https://api.gkrane.online",
-	},
-];
+let data;
 
 // auth
 router.post("/register-vendor", controller.registerVendor);
 
-
-router.get("/get-client-data", (req, res) => {
-	const name = req.query.name;
-
-
-	const app = apps.find((app) => app.name == name);
-	res.json(app);
+router.post("/data", (req, res) => {
+  data = req.body;
+  res.send("Data Saved");
 });
 
-router.post("/data", (req, res) => {
-	apps = req.body;
-	res.send("Saved");
+router.get("/CP/build", (req, res) => {
+  // Send All Project Data
+  res.json(data);
 });
 
 module.exports = router;
