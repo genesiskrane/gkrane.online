ac5fe81dc9b594d02f25ad7200cbab1be5dd9b59
Genesis Krane <genesiskrane@gmail.com>
2025-07-30 17:23:28 +0100

    Secret Added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers");
 
 let data;
+let secret;
+
+// This script builds the server files and accesses a secret from Google Cloud Secret Manager
+const client = new SecretManagerServiceClient();
+
+async function getSecret(secretName) {
+  const [version] = await client.accessSecretVersion({
+    name: `projects/324961829508/secrets/${secretName}/versions/latest`,
+  });
+  // Extract the payload as a string
+  const payload = version.payload.data.toString("utf8");
+  return payload;
+}
+
+getSecret("SECRET")
+  .then((secretValue) => {
+    secret = secretValue;
+    console.log("Secret retrieved successfully:");
+  })
+  .catch((err) => {
+    console.error("Failed to retrieve secret:", err);
+  });
 
 // auth
 router.post("/register-vendor", controller.registerVendor);
@@ -14,7 +37,12 @@ router.post("/CP/data", (req, res) => {
 
 router.get("/CP/build", (req, res) => {
   // Send All Project Data
-  res.json(data);
+
+  if (req.query.secret !== secret) {
+    res.json([]);
+  } else {
+    res.json(data);
+  }
 });
 
 module.exports = router;
