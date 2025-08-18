00fcbcae3ecc685db5bbf46196360d59795b538e
genesiskrane <genesiskrane@gmail.com>
2025-08-18 15:50:20 +0000

    Users Data Sent

---
+++ data/index.js
@@ -0,0 +1,154 @@
+const data = {
+  projects: [
+    {
+      id: "CPanel",
+      name: "Control Panel",
+      repo: "https://github.com/genesiskrane/CPanel",
+      clients: [
+        {
+          default: "https://github.com/Augment-Plus/vite-plus",
+        },
+      ],
+      domains: [],
+      description: "A control panel for managing your projects.",
+    },
+    {
+      id: "unknown",
+      name: "Unknown",
+      repo: "https://github.com/kingujebeh/Express",
+      clients: [
+        {
+          default: "https://github.com/kingujebeh/kingdom",
+        },
+      ],
+      domains: ["augmentplus.space", "africa.world"],
+      description: "Ammadioha(Ammadiamen)",
+    },
+    {
+      id: "currency",
+      name: "Currency",
+      repo: "https://github.com/kingujebeh/currency",
+      clients: [
+        {
+          default: "https://github.com/kingujebeh/kingdom",
+        },
+      ],
+      domains: ["marblelimit.com"],
+      description: "Currency",
+    },
+    {
+      id: "dotpro",
+      name: ".pro Express Server",
+      repo: "https://github.com/Augment-Plus/.pro",
+      clients: [
+        {
+          default: "https://github.com/Augment-Plus/pro",
+        },
+      ],
+      domains: ["creatyve3d.pro"],
+      description: "A .pro server for hosting professional projects.",
+    },
+    {
+      id: "dotstore",
+      name: ".store Express Server",
+      repo: "https://github.com/Augment-Plus/.store",
+      clients: [
+        {
+          default: "https://github.com/Augment-Plus/store",
+        },
+      ],
+      domains: ["yatsar.store"],
+      description: "A .store server for e-commerce projects.",
+    },
+    {
+      id: "dotshop",
+      name: ".shop Express Server",
+      repo: "https://github.com/Augment-Plus/.shop",
+      clients: [
+        {
+          default: "https://github.com/Augment-Plus/shop",
+        },
+      ],
+      domains: ["osasengineering.shop"],
+      description: "A .shop server for online shopping projects.",
+    },
+  ],
+
+  users: [
+    {
+      firstName: "Beauty",
+      lastName: "Yoh",
+      email: "yohbeauty@gmail.com",
+    },
+    {
+      firstName: "Henry",
+      lastName: "Uwajeh",
+      email: "spoiltcooker@gmail.com",
+    },
+    {
+      firstName: "Augment",
+      lastName: "Chinedu",
+      email: "augmentchinedu@gmail.com",
+    },
+    {
+      firstName: "Kingsley",
+      lastName: "Ogbu",
+      email: "thekinggroups1@gmail.com",
+    },
+    {
+      firstName: "Nicholas",
+      lastName: "Uwajeh",
+      email: "nicholasuwajeh@gmail.com",
+    },
+    {
+      firstName: "Elizabeth",
+      lastName: "Uwajeh",
+      email: "lizzymadukauwajeh@gmail.com",
+    },
+    {
+      firstName: "Rotimi",
+      lastName: "Bello",
+      email: "rotimiisrael@gmail.com",
+    },
+    {
+      firstName: "Sunshine",
+      lastName: "O",
+      email: "theyatsarbrand@gmail.com",
+    },
+    {
+      firstName: "Desmond",
+      lastName: "Osaretin",
+      email: "desmondosaretin989@gmail.com",
+    },
+    {
+      firstName: "Othuke",
+      lastName: "Efe",
+      email: "efealltymz@gmail.com",
+    },
+  ],
+  tokens: {},
+  password: "No Password Yet",
+  secrets: [
+    {
+      id: "secret",
+      name: "SECRET",
+    },
+    {
+      id: "password",
+      name: "MONGODB_PASSWORD",
+    },
+    {
+      id: "kingujebeh",
+      name: "KINGUJEBEH",
+    },
+    {
+      id: "augmentplus",
+      name: "AUGMENTPLUS",
+    },
+    {
+      id: "genesiskrane",
+      name: "GENESISKRANE",
+    },
+  ],
+};
+module.exports = data;
--- routes/index.js
+++ routes/index.js
@@ -1,109 +1,10 @@
 const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 const express = require("express");
-const router = express.Router();
+
+const data = require("../data");
 const controller = require("../controllers");
 
-let secrets = [
-  {
-    id: "secret",
-    name: "SECRET",
-  },
-  {
-    id: "password",
-    name: "MONGODB_PASSWORD",
-  },
-  {
-    id: "kingujebeh",
-    name: "KINGUJEBEH",
-  },
-  {
-    id: "augmentplus",
-    name: "AUGMENTPLUS",
-  },
-  {
-    id: "genesiskrane",
-    name: "GENESISKRANE",
-  },
-];
-
-let data = {
-  projects: [
-    {
-      id: "CPanel",
-      name: "Control Panel",
-      repo: "https://github.com/genesiskrane/CPanel",
-      clients: [
-        {
-          default: "https://github.com/Augment-Plus/vite-plus",
-        },
-      ],
-      domains: [],
-      description: "A control panel for managing your projects.",
-    },
-    {
-      id: "unknown",
-      name: "Unknown",
-      repo: "https://github.com/kingujebeh/Express",
-      clients: [
-        {
-          default: "https://github.com/kingujebeh/kingdom",
-        },
-      ],
-      domains: ["augmentplus.space", "africa.world"],
-      description: "Ammadioha(Ammadiamen)",
-    },
-    {
-      id: "currency",
-      name: "Currency",
-      repo: "https://github.com/kingujebeh/currency",
-      clients: [
-        {
-          default: "https://github.com/kingujebeh/kingdom",
-        },
-      ],
-      domains: ["marblelimit.com"],
-      description: "Currency",
-    },
-    {
-      id: "dotpro",
-      name: ".pro Express Server",
-      repo: "https://github.com/Augment-Plus/.pro",
-      clients: [
-        {
-          default: "https://github.com/Augment-Plus/pro",
-        },
-      ],
-      domains: ["creatyve3d.pro"],
-      description: "A .pro server for hosting professional projects.",
-    },
-    {
-      id: "dotstore",
-      name: ".store Express Server",
-      repo: "https://github.com/Augment-Plus/.store",
-      clients: [
-        {
-          default: "https://github.com/Augment-Plus/store",
-        },
-      ],
-      domains: ["yatsar.store"],
-      description: "A .store server for e-commerce projects.",
-    },
-    {
-      id: "dotshop",
-      name: ".shop Express Server",
-      repo: "https://github.com/Augment-Plus/.shop",
-      clients: [
-        {
-          default: "https://github.com/Augment-Plus/shop",
-        },
-      ],
-      domains: ["osasengineering.shop"],
-      description: "A .shop server for online shopping projects.",
-    },
-  ],
-  tokens: {},
-  password: "No Password Yet",
-};
+const router = express.Router();
 
 // This script builds the server files and accesses a secret from Google Cloud Secret Manager
 const client = new SecretManagerServiceClient();
@@ -118,20 +19,20 @@ async function getSecret(secretName) {
 }
 
 (async () => {
-  for (secret of secrets) {
+  for (secret of data.secrets) {
     let secretValue = await getSecret(secret.name);
     secret.value = secretValue;
   }
-  data.tokens["Augment-Plus"] = secrets.find(
+  data.tokens["Augment-Plus"] = data.secrets.find(
     (secret) => secret.name == "AUGMENTPLUS"
   ).value;
-  data.tokens.genesiskrane = secrets.find(
+  data.tokens.genesiskrane = data.secrets.find(
     (secret) => secret.name == "GENESISKRANE"
   ).value;
-  data.tokens.kingujebeh = secrets.find(
+  data.tokens.kingujebeh = data.secrets.find(
     (secret) => secret.name == "KINGUJEBEH"
   ).value;
-  data.password = secrets.find(
+  data.password = data.secrets.find(
     (secret) => secret.name == "MONGODB_PASSWORD"
   ).value;
 })();
@@ -142,12 +43,17 @@ router.post("/register-vendor", controller.registerVendor);
 router.get("/CP/build", (req, res) => {
   // Send All Project Data
   if (
-    req.query.secret === secrets.find((secret) => secret.name == "SECRET").value
+    req.query.secret ===
+    data.secrets.find((secret) => secret.name == "SECRET").value
   ) {
     res.json(data);
   } else {
     res.json([]);
   }
+});
+
+router.get("/users", (req, res) => {
+  res.send(data.users);
 });
 
 module.exports = router;
